8c4d5b3db132c54d08e69f6ca867c26134161d4a
João Rezende <joaoricardorezende@gmail.com>
2025-05-20 14:23:13 -0300

    ChessException and ChessPosition

--- src/application/UI.java
+++ src/application/UI.java
@@ -6,13 +6,13 @@ public class UI {
 
     public static void printBoard(ChessPiece[][] pieces){
         for (int i=0; i < pieces.length; i++){
-            System.out.println(8 - i + " ");
+            System.out.print(8 - i + "  ");
             for (int j=0; j < pieces[i].length; j++){
                 printPiece(pieces[i][j]);
             }
             System.out.println();
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println("   a  b  c  d  e  f  g  h");
     }
 
     private  static void  printPiece(ChessPiece piece) {
@@ -21,6 +21,6 @@ public class UI {
         } else {
             System.out.print(piece);
         }
-        System.out.println(" ");
+        System.out.print("  ");
     }
 }
