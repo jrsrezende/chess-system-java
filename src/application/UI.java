92c6ee4bffceee897fa049b16d2867eb566c7617
João Rezende <joaoricardorezende@gmail.com>
2025-05-20 10:32:22 -0300

    BoardException and defensive programming

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,9 +5,9 @@ import chess.ChessPiece;
 public class UI {
 
     public static void printBoard(ChessPiece[][] pieces){
-        for (int i=0; i < pieces.length; i ++){
+        for (int i=0; i < pieces.length; i++){
             System.out.println(8 - i + " ");
-            for (int j=0; j < pieces[i].length;){
+            for (int j=0; j < pieces[i].length; j++){
                 printPiece(pieces[i][j]);
             }
             System.out.println();
@@ -17,9 +17,9 @@ public class UI {
 
     private  static void  printPiece(ChessPiece piece) {
         if (piece == null) {
-            System.out.println("-");
+            System.out.print("-");
         } else {
-            System.out.println(piece);
+            System.out.print(piece);
         }
         System.out.println(" ");
     }
